f5b44d80eae43c941f16aeb6c271b149f5ab9429
Vitor Rezende <vitor.rezende11@gmail.com>
2020-11-14 21:02:53 -0300

    Templates Config

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import hello_blog
+from django.db import models
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-
+from website.models import Post
 
 def hello_blog(request):
     lista = [
-    'Home', 'Podcast', 'Personagens', 'Cenários', 'RPG',
+        'Home',
+        'Podcast',
+        'Personagens',
+        'Cenários',
+        'RPG',
     ]
-    data = {'Nome': 'Blog Django 3', 'lista_tecnologias': lista}
+
+    list_posts = Post.objects.all()
+
+    data = {
+        'Nome': 'Blog Django 3',
+        'lista_tecnologias': lista,
+        'posts': list_posts
+    }
 
     return render(request, 'index.html', data)
